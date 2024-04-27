0be03b83af47d10980f5a057ef41542dc5e084ec
Karim El-Refai <karim.el-refai@berkeley.edu>
2024-04-27 01:14:44 -0700

    made dumb space stuff

--- train_langsplat.py
+++ train_langsplat.py
@@ -265,6 +265,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
                         best_psnr = test_psnr
                         print("\n[ITER {}] Saving best checkpoint".format(iteration))
                         torch.save((gaussians.capture(), iteration), scene.model_path + "/chkpnt_best.pth")
+
                 if (iteration in saving_iterations):
                     print("\n[ITER {}] Saving Gaussians".format(iteration))
                     scene.save(iteration)
@@ -273,21 +274,21 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
                 # if not opt.include_feature:
                 #     if iteration < opt.densify_until_iter:
                 if iteration < opt.densify_until_iter and (opt.densify_until_num_points < 0 or gaussians.get_xyz.shape[0] < opt.densify_until_num_points):
-                        # Keep track of max radii in image-space for pruning
-                        gaussians.max_radii2D[visibility_filter] = torch.max(gaussians.max_radii2D[visibility_filter], radii[visibility_filter])
-                        # gaussians.add_densification_stats(viewspace_point_tensor, visibility_filter)
-                        if batch_size == 1:
-                            gaussians.add_densification_stats(viewspace_point_tensor, visibility_filter, batch_t_grad if gaussians.gaussian_dim == 4 else None)
-                        else:
-                            gaussians.add_densification_stats_grad(batch_viewspace_point_grad, visibility_filter, batch_t_grad if gaussians.gaussian_dim == 4 else None)
+                    # Keep track of max radii in image-space for pruning
+                    gaussians.max_radii2D[visibility_filter] = torch.max(gaussians.max_radii2D[visibility_filter], radii[visibility_filter])
+                    # gaussians.add_densification_stats(viewspace_point_tensor, visibility_filter)
+                    if batch_size == 1:
+                        gaussians.add_densification_stats(viewspace_point_tensor, visibility_filter, batch_t_grad if gaussians.gaussian_dim == 4 else None)
+                    else:
+                        gaussians.add_densification_stats_grad(batch_viewspace_point_grad, visibility_filter, batch_t_grad if gaussians.gaussian_dim == 4 else None)
 
-                        if iteration > opt.densify_from_iter and iteration % opt.densification_interval == 0:
-                            size_threshold = 20 if iteration > opt.opacity_reset_interval else None
-                            # gaussians.densify_and_prune(opt.densify_grad_threshold, 0.005, scene.cameras_extent, size_threshold)
-                            gaussians.densify_and_prune(opt.densify_grad_threshold, opt.thresh_opa_prune, scene.cameras_extent, size_threshold, opt.densify_grad_t_threshold)
-                        
-                        if iteration % opt.opacity_reset_interval == 0 or (dataset.white_background and iteration == opt.densify_from_iter):
-                            gaussians.reset_opacity()
+                    if iteration > opt.densify_from_iter and iteration % opt.densification_interval == 0:
+                        size_threshold = 20 if iteration > opt.opacity_reset_interval else None
+                        # gaussians.densify_and_prune(opt.densify_grad_threshold, 0.005, scene.cameras_extent, size_threshold)
+                        gaussians.densify_and_prune(opt.densify_grad_threshold, opt.thresh_opa_prune, scene.cameras_extent, size_threshold, opt.densify_grad_t_threshold)
+                    
+                    if iteration % opt.opacity_reset_interval == 0 or (dataset.white_background and iteration == opt.densify_from_iter):
+                        gaussians.reset_opacity()
 
                 # Optimizer step
                 if iteration < opt.iterations:
@@ -464,6 +465,7 @@ if __name__ == "__main__":
         args.test_iterations = args.test_iterations + [i for i in range(0,op.iterations,500)]
     
     setup_seed(args.seed)
+    
     print("Optimizing " + args.model_path)
 
     # Initialize system state (RNG)
