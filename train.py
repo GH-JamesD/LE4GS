734818ec25cc8c4697e027d3a5ec76dc2c511cdc
Karim El-Refai <karim.el-refai@berkeley.edu>
2024-04-27 21:15:05 -0700

    forgot to add the language feature stuff from langsplat train to the merge train.py

--- train.py
+++ train.py
@@ -114,7 +114,8 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
             # Render
             if (iteration - 1) == debug_from:
                 pipe.debug = True
-            
+            render_pkg = render(viewpoint_cam, gaussians, pipe, background, opt)
+            image, language_feature, viewspace_point_tensor, visibility_filter, radii = render_pkg["render"], render_pkg["language_feature_image"], render_pkg["viewspace_points"], render_pkg["visibility_filter"], render_pkg["radii"]
             batch_point_grad = []
             batch_visibility_filter = []
             batch_radii = []
